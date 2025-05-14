00ccc81e2b7d9faed49c1fe7b6fcb59060f21be6
Салаватов Михаил Валерьевич <MVSalavatov@greenatom.ru>
2025-05-14 13:41:37 +0300

    login page tests added

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,15 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+    # link = "http://selenium1py.pythonanywhere.com"
+    # link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    # page = MainPage(browser, link)
+    # page.open()
+    
+    # page.go_to_login_page()
+
+    link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.should_be_login_page()
